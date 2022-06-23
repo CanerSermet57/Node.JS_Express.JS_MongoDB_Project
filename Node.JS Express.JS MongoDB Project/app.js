0d846263068be954d3db6592891f7875aff75e35
Caner <canersermet2@gmail.com>
2022-06-23 19:40:28 +0300

    add and configure express handlebars

--- Node.JS Express.JS MongoDB Project/app.js
+++ Node.JS Express.JS MongoDB Project/app.js
@@ -1,11 +1,33 @@
 const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const hostname = '127.0.0.1'
 const port = 3000
 
 app.use(express.static('public'))
 
+app.engine('handlebars', exphbs.engine());
+app.set('view engine','handlebars')
+
+app.get('/', (req,res) => {
+    res.render('Site/index')
+})
+app.get('/index.html', (req,res) => {
+    res.render('Site/index')
+})
+app.get('/about.html', (req,res) => {
+    res.render('Site/about')
+})
+app.get('/contact.html', (req,res) => {
+    res.render('Site/contact')
+})
+app.get('/blog.html', (req,res) => {
+    res.render('Site/blog')
+})
+
+
+/*
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'Site/index.html'))
 })
@@ -21,6 +43,7 @@ app.get('/about.html', (req,res) => {
 app.get('/contact.html', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'Site/contact.html'))
 })
+*/
 
 app.listen(port,hostname,()=>{
     console.log(` Server Running, http://${hostname}:${port}/`)
