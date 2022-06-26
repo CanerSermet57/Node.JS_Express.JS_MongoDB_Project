d9c91e0aa8bae9076cfbd5dc9213e025a1aded26
Caner <canersermet2@gmail.com>
2022-06-27 00:20:39 +0300

    Register Page

--- Node.JS Express.JS MongoDB Project/app.js
+++ Node.JS Express.JS MongoDB Project/app.js
@@ -13,40 +13,24 @@ app.set('view engine','handlebars')
 app.get('/', (req,res) => {
     res.render('Site/index')
 })
-app.get('/index.html', (req,res) => {
+app.get('/index', (req,res) => {
     res.render('Site/index')
 })
-app.get('/about.html', (req,res) => {
+app.get('/about', (req,res) => {
     res.render('Site/about')
 })
-app.get('/contact.html', (req,res) => {
+app.get('/contact', (req,res) => {
     res.render('Site/contact')
 })
-app.get('/blog.html', (req,res) => {
+app.get('/blog', (req,res) => {
     res.render('Site/blog')
 })
-app.get('/login.html', (req,res) => {
+app.get('/login', (req,res) => {
     res.render('Site/login')
 })
-
-
-/*
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'Site/index.html'))
+app.get('/register', (req,res) => {
+    res.render('Site/register')
 })
-app.get('/index.html', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'Site/index.html'))
-})
-app.get('/blog.html', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'Site/blog.html'))
-})
-app.get('/about.html', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'Site/about.html'))
-})
-app.get('/contact.html', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'Site/contact.html'))
-})
-*/
 
 app.listen(port,hostname,()=>{
     console.log(` Server Running, http://${hostname}:${port}/`)
